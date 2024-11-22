1df6317579af5181b10673a69a5bd7a113498caf
badassmessiah <lashaandguladze69@gmail.com>
2024-11-22 16:24:57 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let burger = document.querySelector(".burger");
+let x_Mark = document.querySelector(".x_Mark");
+let mobileMenu = document.querySelector(".header_nav");
+burger.addEventListener("click", () => {
+  mobileMenu.style.left = "0px";
+  x_Mark.style.display = "block";
+  burger.style.display = "none";
+});
+
+x_Mark.addEventListener("click", () => {
+  mobileMenu.style.left = "-100%";
+  x_Mark.style.display = "none";
+  burger.style.display = "block";
+});
+
